c892a278aa45138ca0cce8867179ff0b6c5077da
Luis <lmongarman@gmail.com>
2021-10-07 14:41:43 +0200

    seguridad

--- 07 - Practica Diferencia Ficheros/src/view/Principal.java
+++ 07 - Practica Diferencia Ficheros/src/view/Principal.java
@@ -11,13 +11,10 @@ import java.io.IOException;
 public class Principal {
 
 	public static void main(String[] args) {
-		
 		File fch01 = new File("f1.txt");
 		File fch02 = new File("f2.txt");
 		File fchResultado = new File("fd.txt");
-		
 		crearFchComun(fch01, fch02, fchResultado);
-
 	}
 
 	private static void crearFchComun(File fch01, File fch02, File fchResultado) {
@@ -25,40 +22,30 @@ public class Principal {
 		try {
 			BufferedReader brFch01 = new BufferedReader(new FileReader(fch01));
 			BufferedWriter bwFchResultado= new BufferedWriter(new FileWriter(fchResultado));
-			
 			String sLinea01 = brFch01.readLine();
-			
 			while (sLinea01 != null) {
 				BufferedReader brFch02 = new BufferedReader(new FileReader(fch02));
 				String sLinea02 = brFch02.readLine();
 				boolean booEncontrado = false;
-				
 				while (sLinea02 != null && !booEncontrado) {
-					
 					if (sLinea01.equals(sLinea02)) {
-						
 						if (iContador > 0) {
 							bwFchResultado.newLine();
 							iContador++;
 						} else {
 							iContador++;
 						}
-						
 						bwFchResultado.write(sLinea01);
 						booEncontrado = true;
-						
-						
 					}
 					sLinea02 = brFch02.readLine();
 				}
 				sLinea01 = brFch01.readLine();
 				brFch02.close();
 			}
-			
 			bwFchResultado.flush();
 			bwFchResultado.close();
 			brFch01.close();
-			
 		} catch (FileNotFoundException e) {
 			System.out.println("El fichero no existe");
 		} catch (IOException ioe){
@@ -66,7 +53,5 @@ public class Principal {
 		} catch (Exception eiu) {
 			System.out.println("Se ha producido un error no tipificado");
 		}
-		
 	}
-
 }
