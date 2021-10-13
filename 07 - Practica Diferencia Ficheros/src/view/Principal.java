abcd59b2c381a7d437510c8aa55a99615b4b5487
Luis <Montero>
2021-10-13 13:38:58 +0200

    Practica invertir

--- 07 - Practica Diferencia Ficheros/src/view/Principal.java
+++ 07 - Practica Diferencia Ficheros/src/view/Principal.java
@@ -18,34 +18,49 @@ public class Principal {
 	}
 
 	private static void crearFchComun(File fch01, File fch02, File fchResultado) {
+		
 		int iContador = 0;
+		
 		try {
 			BufferedReader brFch01 = new BufferedReader(new FileReader(fch01));
 			BufferedWriter bwFchResultado= new BufferedWriter(new FileWriter(fchResultado));
 			String sLinea01 = brFch01.readLine();
+			
 			while (sLinea01 != null) {
+				
+//				if (lineaEncontrada(sLinea01,fch02)) {
+//					escribirLoIGual(sLinea01, fchResultado);
+//				}
+				
 				BufferedReader brFch02 = new BufferedReader(new FileReader(fch02));
 				String sLinea02 = brFch02.readLine();
 				boolean booEncontrado = false;
+				
 				while (sLinea02 != null && !booEncontrado) {
+					
 					if (sLinea01.equals(sLinea02)) {
+						
 						if (iContador > 0) {
 							bwFchResultado.newLine();
 							iContador++;
 						} else {
 							iContador++;
 						}
+						
 						bwFchResultado.write(sLinea01);
 						booEncontrado = true;
+						
 					}
 					sLinea02 = brFch02.readLine();
 				}
 				sLinea01 = brFch01.readLine();
 				brFch02.close();
 			}
+			
 			bwFchResultado.flush();
 			bwFchResultado.close();
 			brFch01.close();
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("El fichero no existe");
 		} catch (IOException ioe){
@@ -53,5 +68,15 @@ public class Principal {
 		} catch (Exception eiu) {
 			System.out.println("Se ha producido un error no tipificado");
 		}
+	}
+	
+	private static boolean lineaEncontrada(File fch01, File fch02, File fchResultado) {
+		boolean booEncontrado = false;
+		
+		return booEncontrado;
+	}
+	
+	private static void escribirLoIGual() {
+		
 	}
 }
---
+++ 08 - Practica Invertir fichero/src/view/Principal.java
@@ -0,0 +1,80 @@
+package view;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		File fch01 = new File("f1.txt");
+		File fchResultado = new File("fresultado.txt");
+		invertirFichero(fch01, fchResultado);
+
+	}
+
+	private static void invertirFichero(File fch01, File fchResultado) {
+
+		int iNumLineas = contadorLineas(fch01);
+		String sLineaLeida = "";
+		boolean booPrimera = true;
+		try {
+			BufferedWriter buffWrite = new BufferedWriter(new FileWriter(fchResultado));
+			for (int i = iNumLineas; i > 0; i--) {
+				BufferedReader buffReader = new BufferedReader(new FileReader(fch01));
+				for (int j = i; j > 0; j--) {
+					sLineaLeida = buffReader.readLine();
+				}
+				if (!booPrimera) {
+					buffWrite.newLine();
+					
+				} else {
+					booPrimera = false;
+				}
+				
+				buffWrite.write(sLineaLeida);
+				buffReader.close();
+			}
+			buffWrite.flush();
+			buffWrite.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("El fichero no existe");
+		} catch (IOException ioe){
+			System.out.println("Se ha producido un error al acceder al fichero");
+		} catch (Exception eiu) {
+			System.out.println("Se ha producido un error no tipificado");
+		}
+
+
+
+	}
+
+	private static int contadorLineas(File fch01) {
+		int iNumLineas = 0;
+		
+		try {
+			BufferedReader buffReader = new BufferedReader(new FileReader(fch01));
+			String sLineaLeida = buffReader.readLine();
+			while (sLineaLeida != null) {
+				iNumLineas++;
+				sLineaLeida = buffReader.readLine();
+			}
+			buffReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("El fichero no existe");
+		} catch (IOException ioe){
+			System.out.println("Se ha producido un error al acceder al fichero");
+		} catch (Exception eiu) {
+			System.out.println("Se ha producido un error no tipificado");
+		}
+
+		return iNumLineas;
+	}
+
+}
