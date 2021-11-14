46f0121559546c573ce6f54ccfa8b254e3c6380d
Luis <Montero>
2021-11-14 19:28:49 +0100

    Hibernate

---
+++ 15 - HIibernate Test/src/model/Coche.java
@@ -0,0 +1,65 @@
+package model;
+
+public class Coche {
+
+	private Integer iIdCoche;
+	private String sMarca;
+	private String sModelo;
+	private Integer iPotencia;
+	
+	public Coche() {
+		this.iIdCoche = null;
+		this.sMarca = "";
+		this.sModelo = "";
+		this.iPotencia = null;
+	}
+	
+	public Coche(String sMarca, String sModelo, Integer iPotencia) {
+		this.sMarca = sMarca;
+		this.sModelo = sModelo;
+		this.iPotencia = iPotencia;
+	}
+
+	public Integer getiIdCoche() {
+		return iIdCoche;
+	}
+
+	public void setiIdCoche(Integer iIdCoche) {
+		this.iIdCoche = iIdCoche;
+	}
+
+	public String getsMarca() {
+		return sMarca;
+	}
+
+	public void setsMarca(String sMarca) {
+		this.sMarca = sMarca;
+	}
+
+	public String getsModelo() {
+		return sModelo;
+	}
+
+	public void setsModelo(String sModelo) {
+		this.sModelo = sModelo;
+	}
+
+	public Integer getiPotencia() {
+		return iPotencia;
+	}
+
+	public void setiPotencia(Integer iPotencia) {
+		this.iPotencia = iPotencia;
+	}
+
+	@Override
+	public String toString() {
+		return "Coche [iIdCoche=" + iIdCoche + ", sMarca=" + sMarca + ", sModelo=" + sModelo + ", iPotencia="
+				+ iPotencia + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ 15 - HIibernate Test/src/view/FrmPrincipal.java
@@ -0,0 +1,34 @@
+package view;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import model.Coche;
+
+public class FrmPrincipal {
+
+	public static void main(String[] args) {
+		
+		org.jboss.logging.Logger logger = org.jboss.logging.Logger.getLogger("org.hibernate");
+		java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.OFF);
+
+		SessionFactory conf = new Configuration().configure().buildSessionFactory();
+		Session session = conf.openSession();
+		
+		Transaction tx = session.beginTransaction();
+		
+		Coche c = new Coche("Tesla","S",0);
+		session.save(c);
+		
+		System.out.println("Añadido el coche: " + c.toString());
+		
+		tx.commit();
+
+		session.close();
+		conf.close();
+
+	}
+
+}
